2123ea0bc7616c197a5a724be598c8cfd2a8fe67
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-04-21 00:52:30 -0300

    refactor: better ui to mascot page

--- app/mascot/page.tsx
+++ app/mascot/page.tsx
@@ -4,12 +4,14 @@ import { useRouter } from 'next/navigation';
 import { Button } from '@/components/button';
 import { Input } from '@/components/input';
 import { Mascot } from '@/components/mascot';
-import { Title } from '@/components/title';
+import { BaseLayout } from '@/components/base-layout';
+import { Paragraph } from '@/components/paragraph';
 import { useMascot } from '@/contexts/mascot';
 
 export default function Maskot() {
   const router = useRouter();
   const { setName } = useMascot();
+  const [loading, setLoading] = useState(false);
   const [inputValue, setInputValue] = useState('');
 
   const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
@@ -17,27 +19,44 @@ export default function Maskot() {
   }, []);
 
   const setMascotName = (event: FormEvent) => {
+    setLoading(true);
     event.preventDefault();
     setName(inputValue);
-    localStorage.setItem('mascotName', inputValue);
     router.push('/scene/1');
   };
 
   return (
-    <main className='flex min-h-screen flex-col items-center justify-start gap-6 p-4 md:p-24 bg-slate-800 text-white'>
-      <Title>Escolha o nome do seu mascote</Title>
-      <Mascot />
-      <form onSubmit={setMascotName} className='flex flex-col gap-2'>
-        <Input
-          type='text'
-          required
-          minLength={3}
-          maxLength={15}
-          onChange={handleChange}
-          value={inputValue}
-        />
-        <Button type='submit'>Nomear</Button>
-      </form>
-    </main>
+    <BaseLayout>
+      <div className='h-full p-8 border-4 border-white rounded-md flex flex-col gap-6'>
+        <Paragraph className='text-center md:text-2xl font-normal'>
+          Para começarmos nossa jornada de entendimento sobre o behaviorismo,
+          essa pequena criatura será nossa companheira e aliada. No entanto,
+          ainda não decidimos qual nome dar a ela. Você teria alguma sugestão?
+        </Paragraph>
+        <div className={loading ? 'opacity-75 animate-pulse' : ''}>
+          <Mascot />
+        </div>
+        <form
+          onSubmit={setMascotName}
+          className='flex flex-col gap-4 justify-center items-center'
+        >
+          <Input
+            className={loading ? 'opacity-75 animate-pulse delay-100' : ''}
+            type='text'
+            required
+            minLength={3}
+            maxLength={15}
+            onChange={handleChange}
+            value={inputValue}
+          />
+          <Button
+            className={loading ? 'opacity-75 animate-pulse delay-150' : ''}
+            type='submit'
+          >
+            Nomear
+          </Button>
+        </form>
+      </div>
+    </BaseLayout>
   );
 }
--- components/input.tsx
+++ components/input.tsx
@@ -6,10 +6,10 @@ interface InputProps
     HTMLInputElement
   > {}
 
-export const Input = (props: InputProps) => {
+export const Input = ({ className, ...props }: InputProps) => {
   return (
     <input
-      className='input input-bordered input-lg w-full max-w-xs p-4 rounded text-black bg-slate-100'
+      className={`input input-bordered input-lg w-full max-w-xs p-4 rounded text-black bg-slate-100 ${className}`}
       {...props}
     />
   );
