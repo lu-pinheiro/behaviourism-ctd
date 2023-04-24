24342fd04a633d449e313fe60bacb4ce104cf1ed
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-04-24 00:15:15 -0300

    refactor: redirects / to /mascot

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import { BaseLayout } from '@/components/base-layout';
-import { HomePage } from '@/components/home-page';
-import { BASE_URL } from '@/utils/base-url';
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  const sceneContent = await fetch(BASE_URL + '/api/intro');
-  let initialScreen;
-  try {
-    initialScreen = await sceneContent.json();
-  } catch {}
-
-  return (
-    <BaseLayout>
-      <HomePage data={initialScreen} />
-    </BaseLayout>
-  );
+  redirect('/mascot');
 }
