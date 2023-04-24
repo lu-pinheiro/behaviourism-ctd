693a6542eff76b29f7da64c7bebb096c4194d4e7
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-04-24 14:01:25 -0300

    feat: add animations

--- app/win/page.tsx
+++ app/win/page.tsx
@@ -3,8 +3,9 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { BaseLayout } from '@/components/base-layout';
 import { Title } from '@/components/title';
-import { useMascot } from '@/contexts/mascot';
 import { ArrowButton } from '@/components/arrow-button';
+import { Confetti } from '@/components/confetti';
+import { useMascot } from '@/contexts/mascot';
 
 export default function WinPage() {
   const [showPage, setShowPage] = useState(false);
@@ -13,8 +14,9 @@ export default function WinPage() {
 
   useEffect(() => {
     const maxScenePage = localStorage.getItem('maxScenePage') ?? '1';
-    if (Number(maxScenePage) < 3) {
-      replace('/question/1');
+    const maxQuestionPage = localStorage.getItem('maxQuestionPage') ?? '1';
+    if (Number(maxScenePage) < 8 && Number(maxQuestionPage) < 4) {
+      replace('/scene/1');
       return;
     }
     setShowPage(true);
@@ -22,9 +24,13 @@ export default function WinPage() {
 
   return showPage ? (
     <BaseLayout>
+      <Confetti />
       <div className='h-full p-8 border-4 border-white rounded-md flex flex-col gap-4 items-center'>
-        <Title>Parabéns, o {name} tem queijo para a vida inteira!</Title>
+        <Title className='animate__animated animate__jackInTheBox'>
+          Parabéns, o {name} tem queijo para a vida inteira!
+        </Title>
         <img
+          className='animate__animated animate__bounceInUp'
           src='/mascot-win.gif'
           alt='Parabéns! Seu mascote agora tem queijo para a vida toda.'
           width={400}
--- components/arrow-button.tsx
+++ components/arrow-button.tsx
@@ -15,8 +15,10 @@ export const ArrowButton = ({ nextPage, direction, ...props }: ButtonProps) => {
         if (typeof nextPage === 'string' && nextPage.length > 0)
           setMaxPage(nextPage);
       }}
-      className={`fixed ${
-        direction === 'right' ? 'right-0 md:right-4' : 'left-0 md:left-4'
+      className={`animate__animated fixed ${
+        direction === 'right'
+          ? 'right-0 md:right-4 animate__fadeInRight'
+          : 'left-0 md:left-4 animate__fadeInLeft'
       } top-0 h-screen rounded-full hover:bg-slate-600/50 p-4 lg:p-10 flex items-center transition-colors`}
       {...props}
     >
